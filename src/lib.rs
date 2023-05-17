f62b8b8c1f9d8375e83818c993bdd47115849834
zbrachinara <zbrachinara@gmail.com>
2023-05-17 07:21:03 -0400

    extract unsafe constvec item copy

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,35 @@
 //! Provides an arrayvec-like type which can be modified at const-time.
 
 use core::{mem::ManuallyDrop, panic};
-use std::{ops::Deref, ptr::addr_of};
+use std::ops::Deref;
+
+/// This is a macro meant for internal use on `ConstVec`. It copies the element at the given index
+/// to the stack, not modifying the original index.
+/// 
+/// # Safety
+///
+/// At the time of writing, there are a lot of limitations around const. In this case, the relevant
+/// issue is that we are not allowed to get references to objects which may contain [`UnsafeCell`]s
+/// -- both `&self` and `&self.xs` are impossible. This is used to do operations such as remove,
+/// remove_swap, or pop, which would usually require a reference to `self.xs` at least in order to
+/// copy its contents onto the stack. Thus if T contains an `UnsafeCell` this approach probably
+/// isn't allowed, or maybe it just isn't allowed at all because of the strange things we do with a
+/// zst pointer.
+///
+/// And of course, since this function performs a copy of a non-copy type, you need to make sure
+/// that *the element at this index is never accessed as a `T` again*.
+macro_rules! copy_item {
+    ($self:ident, $ix:expr) => {{
+        // we can't get a pointer to xs or self, but we can get one to a zst with the same address
+        let ptr_to_xs = core::ptr::addr_of!($self.xs_addr) as *const T;
+        // we have a pointer to our array now, but we need a pointer to the item's location
+        let ptr_to_elem = ptr_to_xs.add($ix);
+        // now we have enough information to get a slice containing the item
+        let item_as_u8_slice = core::slice::from_raw_parts(ptr_to_elem as *const u8, Self::T_SIZE);
+        // and then use an improvised transmute_copy to obtain the item
+        core::ptr::read_unaligned(item_as_u8_slice as *const _ as *const T)
+    }};
+}
 
 pub struct CapacityError<T, const CAP: usize> {
     pub vector: ConstVec<T, CAP>,
@@ -72,18 +100,9 @@ impl<T, const CAP: usize> ConstVec<T, CAP> {
     /// which have [`UnsafeCell`]s in them (which shouldn't be hard anyway, given that const
     /// disallows heap allocations).
     pub const unsafe fn pop_unchecked(mut self) -> (Self, T) {
-        // we can't get a pointer to xs or self, but we can get a one to a zst with the same address 
-        let ptr_to_xs = addr_of!(self.xs_addr) as *const u8;
-        // we have a pointer to our array now, but we need a pointer to the item's location
-        let ptr_to_elem = ptr_to_xs.add(Self::T_SIZE * (self.len - 1));
-        // now we have enough information to get a slice containing the item
-        let item_as_u8_slice = core::slice::from_raw_parts(ptr_to_elem as *const u8, Self::T_SIZE);
-        // and then use an improvised transmute_copy to obtain the item
-        let item = std::ptr::read_unaligned(item_as_u8_slice as *const _ as *const T);
-        // now that we aren't using the pointers anymore, reduce the length
-        let len = self.len;
-        self = self.set_len(len - 1);
-
+        let new_len = self.len - 1;
+        let item = copy_item!(self, new_len);
+        self = self.set_len(new_len);
         (self, item)
     }
 
