28370b9bfb5171698cab5146b520c3244070e319
zbrachinara <zbrachinara@gmail.com>
2023-05-18 18:27:20 -0400

    test case for const assertions

--- tests/new.rs
+++ tests/new.rs
@@ -1,6 +1,6 @@
 use const_push::ConstVec;
 
-const fn construct_from_array() -> ConstVec<u32, 10>{
+const fn construct_from_array() -> ConstVec<u32, 10> {
     ConstVec::from_array([10, 20, 30])
 }
 const CONSTRUCT_FROM_ARRAY: ConstVec<u32, 10> = construct_from_array();
@@ -8,3 +8,12 @@ const CONSTRUCT_FROM_ARRAY: ConstVec<u32, 10> = construct_from_array();
 fn test_construct_from_array() {
     assert_eq!(CONSTRUCT_FROM_ARRAY.as_slice(), &[10, 20, 30]);
 }
+
+// the below should not compile
+// const fn construct_too_large() -> ConstVec<u32, 1> {
+//     ConstVec::from_array([10, 20])
+// }
+// const CONSTRUCT_TOO_LARGE: ConstVec<u32, 1> = construct_too_large();
+// fn test_construct_too_large() {
+//     let _ = CONSTRUCT_TOO_LARGE;
+// }
