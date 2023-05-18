27951a2e31c1fe5e0f36704e526519f22bb274c2
zbrachinara <zbrachinara@gmail.com>
2023-05-18 19:40:57 -0400

    bypass lint for const assertions

--- src/assertions.rs
+++ src/assertions.rs
@@ -5,6 +5,7 @@ impl<const LESSER: usize, const GREATER: usize> Leq<LESSER, GREATER> {
     const CHECK: () = assert!(LESSER <= GREATER);
 
     pub const fn assert() -> Self {
+        #[allow(clippy::let_unit_value)]
         let _ = Self::CHECK;
         Self {}
     }
